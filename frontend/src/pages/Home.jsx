b8ea43c0da94830ef78bb35bc16da09dc6266e66
UnesABA <unesaitbenali@gmail.com>
2025-04-25 10:19:50 +0100

    Implement featured doctor image with appointment info overlay

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import doctor4          from "../assets/images/Doctor4.jpg"
 import icon01           from "../assets/images/icon01.png"
 import icon02           from "../assets/images/icon02.png"
 import icon03           from "../assets/images/icon03.png"
+import videoIcon        from "../assets/images/video-icon.png"
+import doctor10       from "../assets/images/doctor10.jpg"
+import avatarIcon       from "../assets/images/avatar-icon.png"
 import { Link }         from "react-router-dom"
 import { BsArrowRight } from "react-icons/bs"
 import About            from "../components/About/About"
@@ -161,6 +164,68 @@ const Home = () => {
           </div>
         </section>
       {/* ============== Services Section End ============== */}
+
+      {/* ============== Feature Section Start ============== */}
+        <section>
+          <div className="container">
+            <div className="flex flex-col items-center justify-between lg:flex-row">
+
+              {/* ============== Feature Content ============== */}
+              <div className="xl:w-[670px]">
+                <h2 className= "heading">
+                  Get virtual treatment <br /> anytime
+                </h2>
+                <ul className="pl-4">
+                  <li className="text__para">
+                    1. Schedule the appointment directly
+                  </li>
+                  <li className="text__para">
+                    2. Search for your physician here, and contact their office
+                  </li>
+                  <li className="text__para">
+                    3. View our physicians who are accepting new patients. Use the online scheduling tool to select an appointment time
+                  </li>
+                </ul>
+                <Link to= "/">
+                  <button className="btn">
+                    Learn More
+                  </button>
+                </Link>
+              </div>
+
+              {/* ============== Feature Img ============== */}
+              <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] mr-[50px] lg:mt-0 ">
+                <img src= {doctor10} className= "w-[400px] rounded mr-[130px]"/>
+                <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px] ">
+                  <div className="flex items-center justify-between">
+                    <div className= "flex items-center gap-[6px] lg-gap-3">
+                      <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                        Thur, 24
+                      </p>
+                      <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[400]">
+                        08:00 AM
+                      </p>
+                    </div>
+                    <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
+                      <img src= {videoIcon} />
+                    </span>
+                  </div>
+
+                  <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
+                    Consultation
+                  </div>
+
+                  <div className= "flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px] ">
+                    <img className= "w-[40px] h-[40px] object-cover rounded-[20px]" src={doctor10} />
+                    <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor ">Said AIT BEN ALI</h4>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+      {/* ============== Feature Section End ============== */}
 
 
     </div>
