4dab84fee272d69d5e72c0fbc6233628446a6644
UnesABA <unesaitbenali@gmail.com>
2025-04-27 09:44:48 +0100

    Update Doctors and DoctorsDetails pages with search, testimonials, and tabs

--- frontend/src/assets/data/doctor.js
+++ frontend/src/assets/data/doctor.js
@@ -1,22 +1,25 @@
-import doctor15 from "../images/doctor15.jpg"
+import doctor   from "../images/about1.jpg"
 import doctor2  from "../images/doctor2.jpg"
-import doctor8  from "../images/doctor13.jpg"
+import doctor13 from "../images/doctor13.jpg"
+import doctor17 from "../images/doctor17.jpg"
+import doctor18 from "../images/doctor18.jpg"
+import doctor5  from "../images/doctor5.jpg"
 
 export const doctors = [
   {
-    id            : "01",
+    id            : "03",
     name          : "Dr. Said AIT BEN ALI",
     specialization: "Neurologist",
     avgRating     : 4.8,
     totalRating   : 6455,
-    photo         : doctor8,
+    photo         : doctor13,
     totalPatients : 68506,
     hospital      : "AIT BEN ALI Hospital, Casablanca.",
   },
   {
     id            : "02",
     name          : "Dr. Sliman FARAJI",
-    specialization: "Surgeon",
+    specialization: "Dermatologist",
     avgRating     : 4.3,
     totalRating   : 986,
     photo         : doctor2,
@@ -24,13 +27,44 @@ export const doctors = [
     hospital      : "AIT BEN ALI Hospital, Casablanca.",
   },
   {
-    id            : "03",
-    name          : "Dr. Nisrine KEBBAJ",
-    specialization: "Dermatologist",
+    id            : "01",
+    name          : "Dr. Nabila LMAANI",
+    specialization: "Surgeon",
     avgRating     : 4.1,
     totalRating   : 654,
-    photo         : doctor15,
+    photo         : doctor,
     totalPatients : 1266,
     hospital      : "AIT BEN ALI Hospital, Casablanca.",
-  },
+  }
+  // },
+  // {
+  //   id            : "04",
+  //   name          : "Dr. Zindana KONE",
+  //   specialization: "Dermatologist",
+  //   avgRating     : 4.1,
+  //   totalRating   : 6544,
+  //   photo         : doctor17,
+  //   totalPatients : 1266,
+  //   hospital      : "AIT BEN ALI Hospital, Casablanca.",
+  // },
+  // {
+  //   id            : "04",
+  //   name          : "Dr. Narjis AIT HADDOU",
+  //   specialization: "Oncologist",
+  //   avgRating     : 4.5,
+  //   totalRating   : 6845,
+  //   photo         : doctor18,
+  //   totalPatients : 1266,
+  //   hospital      : "AIT BEN ALI Hospital, Casablanca.",
+  // },
+  // {
+  //   id            : "06",
+  //   name          : "Dr. soukaina NAJJAR",
+  //   specialization: "Pediatrician",
+  //   avgRating     : 4.9,
+  //   totalRating   : 699,
+  //   photo         : doctor5,
+  //   totalPatients : 1266,
+  //   hospital      : "AIT BEN ALI Hospital, Casablanca.",
+  // },
 ]
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -124,7 +124,7 @@ const Home = () => {
 
                 <Link
                   to="/doctors"
-                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none "
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
                   <BsArrowRight className="group-hover:text-white w-6 h-5" />
                 </Link>
