557e2b38caf4565e5cdc74104e8c008b5b8d6772
UnesABA <unesaitbenali@gmail.com>
2025-05-05 19:17:11 +0100

    Add user controller with CRUD operations (update, delete, get one, get all)

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,52 @@
+import User from "../models/UserSchema.js"
+
+export const updateUser = async (req, res) =>{
+  const id = req.params.id
+
+  try {
+    const updateUser = await User.findByIdAndUpdate(id, {$set: req.body}, {new: true})
+
+    res.status(200).json({message: "Successfully Updated", data: updateUser})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({message: "Failed to Update"})
+  }
+}  
+
+export const deleteUser = async (req, res) =>{
+  const id = req.params.id
+
+  try {
+    await User.findByIdAndDelete(id)
+
+    res.status(200).json({message: "Successfully Deleted"})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({message: "Failed to Delete"})
+  }
+} 
+
+export const getSingleUser = async (req, res) =>{
+  const id = req.params.id
+
+  try {
+   const user = await User.findById(id)
+
+    res.status(200).json({message: "User Found", data: user})
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({message: "No User Found"})
+  }
+}
+
+export const getAllUsers = async (req, res) =>{
+
+  try {
+   const users = await User.find()
+
+    res.status(200).json({message: "Users Found", data: users})
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({message: "Users not Found"})
+  }
+}
