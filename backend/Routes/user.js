9b0a769f2ec12e426c2869e77835ba1b95c616df
UnesABA <unesaitbenali@gmail.com>
2025-05-13 16:57:48 +0100

    Add getMyAppointments and getUserProfile controllers to user routes

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,12 +1,21 @@
-import express                                                from "express"
-import { updateUser, deleteUser, getSingleUser, getAllUsers } from "../Controllers/userController.js"
-import { authentificate, restrict }                           from "../auth/verifyToken.js"
+import express from "express"
+import {
+  updateUser,
+  deleteUser,
+  getSingleUser,
+  getAllUsers,
+  getMyAppointments,
+  getUserProfile,
+} from "../Controllers/userController.js"
+import { authentificate, restrict } from "../auth/verifyToken.js"
 
 const route = express.Router()
 
-route.get("/:id"   , authentificate, restrict(["patient"]), getSingleUser)
-route.get("/"      , authentificate, restrict(["admin"])  , getAllUsers)
-route.put("/:id"   , authentificate, restrict(["patient"]), updateUser)
-route.delete("/:id", authentificate, restrict(["patient"]), deleteUser)
+route.get("/:id"                         , authentificate, restrict(["patient"]), getSingleUser)
+route.get("/"                            , authentificate, restrict(["admin"])  , getAllUsers)
+route.put("/:id"                         , authentificate, restrict(["patient"]), updateUser)
+route.delete("/:id"                      , authentificate, restrict(["patient"]), deleteUser)
+route.get("/appointments/my-appointments", authentificate, restrict(["patient"]), getMyAppointments)
+route.get("/profile/me"                  , authentificate, restrict(["patient"]), getUserProfile)
 
 export default route
