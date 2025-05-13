faac84c1cfa6cbc74bead548cdf85a02e3bddfa4
UnesABA <unesaitbenali@gmail.com>
2025-05-13 13:04:41 +0100

    Set getting user profile functionality

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,52 +1,74 @@
 import User from "../models/UserSchema.js"
 
-export const updateUser = async (req, res) =>{
+export const updateUser = async (req, res) => {
   const id = req.params.id
 
   try {
-    const updateUser = await User.findByIdAndUpdate(id, {$set: req.body}, {new: true})
+    const updateUser = await User.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    )
 
-    res.status(200).json({message: "Successfully Updated", data: updateUser})
+    res.status(200).json({ message: "Successfully Updated", data: updateUser })
   } catch (error) {
     console.log(error)
-    res.status(500).json({message: "Failed to Update"})
+    res.status(500).json({ message: "Failed to Update" })
   }
-}  
+}
 
-export const deleteUser = async (req, res) =>{
+export const deleteUser = async (req, res) => {
   const id = req.params.id
 
   try {
     await User.findByIdAndDelete(id)
 
-    res.status(200).json({message: "Successfully Deleted"})
+    res.status(200).json({ message: "Successfully Deleted" })
   } catch (error) {
     console.log(error)
-    res.status(500).json({message: "Failed to Delete"})
-  }
-} 
-
-export const getSingleUser = async (req, res) =>{
-  const id = req.params.id
-
-  try {
-   const user = await User.findById(id).select("-password")
-
-    res.status(200).json({message: "User Found", data: user})
-  } catch (error) {
-    console.log(error)
-    res.status(404).json({message: "No User Found"})
+    res.status(500).json({ message: "Failed to Delete" })
   }
 }
 
-export const getAllUsers = async (req, res) =>{
+export const getSingleUser = async (req, res) => {
+  const id = req.params.id
 
   try {
-   const users = await User.find().select("-password")
+    const user = await User.findById(id).select("-password")
 
-    res.status(200).json({message: "Users Found", data: users})
+    res.status(200).json({ message: "User Found", data: user })
   } catch (error) {
     console.log(error)
-    res.status(404).json({message: "Users not Found"})
+    res.status(404).json({ message: "No User Found" })
+  }
+}
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password")
+
+    res.status(200).json({ message: "Users Found", data: users })
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({ message: "Users not Found" })
+  }
+}
+
+export const getUserProfile = async (req, res) => {
+  const userId = req.userId
+
+  try {
+    const user = await User.findById(userId)
+
+    if (!user) {
+      return res.status(404).json("User not Found")
+    }
+
+    const { password, ...rest } = user._doc
+    res
+      .status(200)
+      .json({ message: "Profile info is getting : ", data: { ...rest } })
+  } catch (error) {
+    res.status(500).json("Something went wrong !")
   }
 }
