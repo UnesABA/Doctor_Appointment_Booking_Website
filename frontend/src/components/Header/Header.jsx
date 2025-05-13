f1fd4adf417d8755f6124af644acdcba1fe01156
UnesABA <unesaitbenali@gmail.com>
2025-05-13 12:18:44 +0100

    Store user, token, and role in localStorage after login

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,38 +1,44 @@
-import logo                  from "../../assets/images/logo.png"
-import Younes                from "../../assets/images/younes.jpg"
-import {NavLink, Link}       from "react-router-dom"
-import {BiMenu}              from "react-icons/bi"
-import { useEffect, useRef } from "react"
+import logo                              from "../../assets/images/logo.png"
+import Younes                            from "../../assets/images/younes.jpg"
+import { NavLink, Link }                 from "react-router-dom"
+import { BiMenu }                        from "react-icons/bi"
+import { useContext, useEffect, useRef } from "react"
+import { authContext }                   from "../../context/AuthContext.jsx"
 
 const navLinks = [
   {
-    path   : "/home",
-    display: "Home"
+    path: "/home",
+    display: "Home",
   },
   {
-    path   : "/doctors",
-    display: "Find a Doctor"
+    path: "/doctors",
+    display: "Find a Doctor",
   },
   {
-    path   : "/services",
-    display: "Services"
+    path: "/services",
+    display: "Services",
   },
   {
-    path   : "/contact",
-    display: "Contact"
+    path: "/contact",
+    display: "Contact",
   },
 ]
- 
+
 const Header = () => {
   const headerRef = useRef(null)
-  const menuRef   = useRef(null)
+  const menuRef = useRef(null)
 
-  useEffect(() =>{
-    const handleStickyHeader = () =>{
-      window.addEventListener("scroll", () =>{
-        if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+  const { user, token, role } = useContext(authContext)
+
+  useEffect(() => {
+    const handleStickyHeader = () => {
+      window.addEventListener("scroll", () => {
+        if (
+          document.body.scrollTop > 80 ||
+          document.documentElement.scrollTop > 80
+        ) {
           headerRef.current.classList.add("sticky__header")
-        }else{
+        } else {
           headerRef.current.classList.remove("sticky__header")
         }
       })
@@ -43,54 +49,62 @@ const Header = () => {
     return () => window.removeEventListener("scroll", handleStickyHeader)
   }, [])
 
-  const toggleMenu = () =>{
+  const toggleMenu = () => {
     menuRef.current.classList.toggle("show__menu")
   }
 
   return (
-    <header className= "header flex items-center" ref= {headerRef} >
+    <header className="header flex items-center" ref={headerRef}>
       <div className="container">
         <div className="flex items-center justify-between">
           {/* ================ Logo ================ */}
           <div>
-            <img src= {logo} alt="logo" />
+            <img src={logo} alt="logo" />
           </div>
 
           {/* ================ Menu ================ */}
-          <div className= "navigation" ref= {menuRef} onClick= {toggleMenu}>
-            <ul className= "menu flex items-center gap-[2.7rem]">
-              {navLinks.map((link, index) =>
-                  <li key= {index}>
-                    <NavLink to= {link.path} className= {({ isActive }) => (isActive 
-                                                                            ? "text-primaryColor text-[16px] leading-7 font-[600]" 
-                                                                            : "text-textColor text-[16px] leading-7 font-[500] hover: text-primaryColor")}>
-                      {link.display}
-                    </NavLink>
-                  </li>
-              )}
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={link.path}
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                        : "text-textColor text-[16px] leading-7 font-[500] hover: text-primaryColor"
+                    }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
           </div>
 
           {/* ================ Nav Right ================ */}
-          <div className= "flex items-center gap-4">
-            <div className= "hidden">
-              <Link to= "/">
+          <div className="flex items-center gap-4">
+            { token && user ? (
+            <div>
+              <Link to= {`${role === "patient" ? "/users/profile/me" : "/doctors/profile/me"}`}>
                 <figure className="w-[35px] h-[35px] rounded-[16px] overflow-hidden">
-                  <img src= {Younes} className= "w-full rounded-full" />
+                  <img src={user?.photo} className="w-full rounded-full" />
                 </figure>
+                <h1>{user?.name}</h1>
+                <img src= {user?.photo} />
               </Link>
-            </div>
-            <Link to= "/login">
-              <button className= "bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px] m-1">
+            </div>) :( 
+            <Link to="/login">
+              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px] m-1">
                 Login
               </button>
-            </Link>
+            </Link>)
+            }
 
-            <span className="md:hidden" onClick= {toggleMenu}>
-              <BiMenu className= "w-6 h-6 cursor-pointer"/>
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
             </span>
           </div>
-
         </div>
       </div>
     </header>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate}     from "react-router-dom"
 import { BASE_URL }             from "../../config.js"
 import { toast }                from "react-toastify"
 import {authContext}            from "../context/AuthContext.jsx"
+import HashLoader from "react-spinners/hashLoader.js"
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -35,8 +36,7 @@ const Login = () => {
         })
   
         const result = await res.json()
-        console.log(result)
-  
+
         if(!res.ok){
           throw new Error(data.message || "Login failed")
         }
@@ -97,7 +97,7 @@ const Login = () => {
               type="submit"
               className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg font-semibold px-4 py-3"
             >
-              Login
+              {loading ? <HashLoader size= {25} color= "#fff" /> : "Login"}
             </button>
           </div>
 
