94922c579585f09a9d84edeb595c2d94359ce6cc
UnesABA <unesaitbenali@gmail.com>
2025-05-13 16:43:14 +0100

    Implement getMyAppointments controller to fetch all doctors linked to a user's bookings

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,4 +1,6 @@
 import User from "../models/UserSchema.js"
+import Booking from "../models/BookingSchema.js"
+import Doctor from "../models/DoctorSchema.js"
 
 export const updateUser = async (req, res) => {
   const id = req.params.id
@@ -68,6 +70,25 @@ export const getUserProfile = async (req, res) => {
     res
       .status(200)
       .json({ message: "Profile info is getting : ", data: { ...rest } })
+  } catch (error) {
+    res.status(500).json("Something went wrong !")
+  }
+}
+
+export const getMyAppointments = async (req, res) => {
+  try {
+    const bookings = await Booking.find({ user: req.userId })
+
+    const doctorIds = bookings.map((element) => {
+      return element.doctor.id
+    })
+    
+    const doctors = await Doctor.find({ _id: { $in: doctorIds } }).select(
+      "-password"
+    )
+    res
+      .status(200)
+      .json({ message: "Appointments are getting : ", data: { doctors }})
   } catch (error) {
     res.status(500).json("Something went wrong !")
   }
